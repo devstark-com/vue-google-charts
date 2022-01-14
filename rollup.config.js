f32ab6971c4c73021528cdce1e586b9236c1f2df
ilajosmanov <illiaosmanov@gmail.com>
2022-01-14 13:02:17 +0200

    chore: added swc and external

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,58 @@
-import vue from 'rollup-plugin-vue'
-import replace from "@rollup/plugin-replace";
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+  import vue from 'rollup-plugin-vue';
+  import swc from 'rollup-plugin-swc';
+  import replace from "@rollup/plugin-replace";
+  import commonjs from '@rollup/plugin-commonjs';
+  import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 import pkg from './package.json';
 
-const commonPlugins = [
+const extensions = ['.js', '.ts'];
+const external = _ => /node_modules/.test(_) && !/@swc\/helpers/.test(_);
+
+const plugins = targets => [
+  nodeResolve({
+    extensions
+  }),
   replace({
     preventAssignment: true,
     values: {
       'import.meta.env.VERSION': JSON.stringify(pkg.version)
     }
   }),
+  swc({
+    env: {
+      targets
+    },
+    module: {
+      type: "es6"
+    },
+    sourceMaps: true
+  })
 ]
 
 export default [
   {
+    external,
     input: 'src/index.js',
     output: {
       format: 'esm',
       file: pkg.module
     },
     plugins: [
-      ...commonPlugins,
-      vue()
+      vue(),
+      ...plugins("defaults and supports es6-module"),
     ]
   },
   {
+    external,
     input: 'src/index.js',
     output: {
       format: 'cjs',
       file: pkg.main
     },
     plugins: [
-      ...commonPlugins,
-      vue({ template: { optimizeSSR: true } })
+      vue({ template: { optimizeSSR: true } }),
+      ...plugins("defaults, not ie 11, not ie_mob 11"),
     ]
   },
   {
@@ -44,10 +62,9 @@ export default [
       file: pkg.unpkg
     },
     plugins: [
-      ...commonPlugins,
-      nodeResolve(),
+      vue(),
+      ...plugins("defaults, not ie 11, not ie_mob 11"),
       commonjs(),
-      vue()
     ]
   }
 ]
