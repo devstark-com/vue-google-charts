d0885971873da59ed3866b8bcd41965b0c45be39
dangreen <danon0404@gmail.com>
2022-01-18 20:34:17 +0700

    chore: cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,10 @@ import swc from 'rollup-plugin-swc';
 import replace from "@rollup/plugin-replace";
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-
 import pkg from './package.json';
 
 const extensions = ['.js', '.ts'];
 const external = _ => /node_modules/.test(_) && !/@swc\/helpers/.test(_);
-
 const plugins = (targets, vueOptions = {}) => [
   vue(vueOptions),
   nodeResolve({
@@ -33,40 +31,36 @@ const plugins = (targets, vueOptions = {}) => [
 
 export default [
   {
-    external,
     input: 'src/index.js',
+    plugins: plugins("defaults and supports es6-module"),
+    external,
     output: {
       format: 'esm',
       file: pkg.module
     },
-    plugins: [
-      ...plugins("defaults and supports es6-module"),
-    ]
   },
   {
-    external,
     input: 'src/index.js',
+    plugins: plugins("defaults, not ie 11, not ie_mob 11", {
+      template: {
+        optimizeSSR: true
+      }
+    }),
+    external,
     output: {
       format: 'cjs',
       file: pkg.main
     },
-    plugins: [
-      ...plugins("defaults, not ie 11, not ie_mob 11", {
-        template: {
-          optimizeSSR: true
-        }
-      }),
-    ]
   },
   {
     input: 'src/umd.js',
-    output: {
-      format: 'umd',
-      file: pkg.unpkg
-    },
     plugins: [
       ...plugins("defaults, not ie 11, not ie_mob 11"),
       commonjs(),
-    ]
+    ],
+    output: {
+      format: 'umd',
+      file: pkg.unpkg
+    }
   }
 ]
