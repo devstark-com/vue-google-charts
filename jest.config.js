9be7eced4fc403163e3cbc28416fcb6c9235cfda
v.terekhov <v.terekhov@iportal.ru>
2022-04-14 21:43:33 +0400

    test: move babel-jest config to jest config file

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,13 @@ module.exports = {
   transform: {
     '^.+\\.vue$': '@vue/vue2-jest',
     // TODO: replace with @swc/jest after code migration to typescript
-    '\\.js$': ['babel-jest', { configFile: './babel-jest.config.js' }],
+    '\\.js$': [
+      'babel-jest',
+      {
+        presets: ['@vue/cli-plugin-babel/preset'],
+        plugins: ['babel-plugin-transform-import-meta'],
+      },
+    ],
   },
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/*'],
