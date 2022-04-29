1ae9613e202f82f94a27bb89a2167b20c92f67bb
v.terekhov <v.terekhov@iportal.ru>
2022-04-29 14:21:19 +0400

    chore: tune configs

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testMatch: ['<rootDir>/test/(*.)spec.(js|ts)'],
   moduleFileExtensions: ['js', 'ts', 'vue'],
   transform: {
-    '^.+\\.vue$': '@vue/vue2-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
     '^.+\\.(t|j)sx?$': [
       '@swc/jest',
       {
